d2fbbbb0cadb739cf05783186106809e8b0e579c
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-04-07 18:33:07 -0300

    Enquanto o número informado não for par entre 4 e 14, um novo prompt pedirá um novo número

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 let numeroDeCartas = 
 prompt("Bem-vindo, com quantas cartas você gostaria de jogar? O Número inserido deve ser par entre 4 e 14.");
 
+while(!(numeroDeCartas % 2 === 0 && numeroDeCartas > 3 && numeroDeCartas < 15)){
+    numeroDeCartas = 
+    prompt("Bem-vindo, com quantas cartas você gostaria de jogar? O Número inserido deve ser par entre 4 e 14.");
+}
+
