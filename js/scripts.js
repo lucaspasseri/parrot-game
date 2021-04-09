87d463956f4e953a1a88460edc70e3656f30e061
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-04-09 03:10:57 -0300

    Ao virar um carta, ela permanece virada até a próxima ser selecionada; Os pares encontrados permanecem virados; Duas cartas que não são pares desviram depois de 1 segundo; Problema: É possível virar mais de duas cartas.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-let numeroDeCartas = 
-prompt("Bem-vindo, com quantas cartas você gostaria de jogar? O Número inserido deve ser par entre 4 e 14.");
+/* let numeroDeCartas = 
+prompt("Bem-vindo, com quantas cartas você gostaria de jogar? O Número inserido deve ser par entre 4 e 14."); */
+
+numeroDeCartas = 8;
 
 while(!(numeroDeCartas % 2 === 0 && numeroDeCartas > 3 && numeroDeCartas < 15)){
     numeroDeCartas = 
@@ -7,38 +9,85 @@ while(!(numeroDeCartas % 2 === 0 && numeroDeCartas > 3 && numeroDeCartas < 15)){
 }
 
 const seletorJogo = document.querySelector(".jogo");
+
 for(let i = 0; i < numeroDeCartas; i++){
-    seletorJogo.innerHTML += `<div class="carta" onclick="virarCarta(this, ${i})">
+    seletorJogo.innerHTML += `<div class="carta" onclick="selecionarCarta(this, ${i})">
                                  <img src="./imgs/front.png">     
                               </div>`;
 }
-
-srcImgsArray = [];
-srcImgsArray.push("./imgs/bobrossparrot.gif");
-srcImgsArray.push("./imgs/bobrossparrot.gif");
-srcImgsArray.push("./imgs/explodyparrot.gif");
-srcImgsArray.push("./imgs/explodyparrot.gif");
-srcImgsArray.push("./imgs/fiestaparrot.gif");
-srcImgsArray.push("./imgs/fiestaparrot.gif");
-srcImgsArray.push("./imgs/metalparrot.gif");
-srcImgsArray.push("./imgs/metalparrot.gif");
-srcImgsArray.push("./imgs/revertitparrot.gif");
-srcImgsArray.push("./imgs/revertitparrot.gif");
-srcImgsArray.push("./imgs/tripletsparrot.gif");
-srcImgsArray.push("./imgs/tripletsparrot.gif");
-srcImgsArray.push("./imgs/unicornparrot.gif");
-srcImgsArray.push("./imgs/unicornparrot.gif");
-
-let srcImgsArrayJogo = srcImgsArray.slice(0,numeroDeCartas);
+srcImgsArray = [
+    "./imgs/bobrossparrot.gif",
+    "./imgs/explodyparrot.gif",
+    "./imgs/fiestaparrot.gif",
+    "./imgs/metalparrot.gif",
+    "./imgs/revertitparrot.gif",
+    "./imgs/tripletsparrot.gif",
+    "./imgs/unicornparrot.gif"
+]
 
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-srcImgsArrayJogo.sort(comparador);
+srcImgsArray.sort(comparador);
 
-console.log("srcImgsArrayJogo:", srcImgsArrayJogo);
-function virarCarta(cartaClickada, index){
-    cartaClickada.querySelector("img").setAttribute("src", srcImgsArrayJogo[index]);
+let srcPairImgsArray = [];
+
+for(let i = 0; i < srcImgsArray.length; i++){
+    srcPairImgsArray.push(srcImgsArray[i]);
+    srcPairImgsArray.push(srcImgsArray[i]);
 }
 
+let srcImgsInGameArray = srcPairImgsArray.slice(0, numeroDeCartas);
+
+srcImgsInGameArray.sort(comparador);
+
+const seletorCartas = document.querySelectorAll(".carta");
+
+let numeroDeCartasSemPar = 0; // não pode ser maior que 2
+
+function virarCarta(carta, index){
+    carta.classList.add("paraCima");
+    carta.querySelector("img").setAttribute("src",srcImgsInGameArray[index]);
+}
+
+function desvirarCarta(carta){
+    carta.classList.remove("paraCima");
+    carta.querySelector("img").setAttribute("src", "./imgs/front.png");
+}
+
+function zerarNumeroDeCartasSemPar(){
+    numeroDeCartasSemPar = 0;
+}
+
+function selecionarCarta(carta, index){
+    if(!carta.classList.contains("paraCima") && numeroDeCartasSemPar === 0){ //se a carta não esta para cima e nenhuma carta semPar
+        virarCarta(carta, index); // vira a carta
+        carta.classList.add("semPar"); // adiciona a classe semPar
+        numeroDeCartasSemPar ++; // número de cartas semPar incrementado
+    } else if (!carta.classList.contains("paraCima") &&  numeroDeCartasSemPar === 1 ) { // se a carta não esta para cima e tem uma carta semPar
+        virarCarta(carta, index);
+        //carta.classList.add("semPar");
+        //numeroDeCartasSemPar ++;
+        for(let i = 0; i < numeroDeCartas; i++){ 
+            if(seletorCartas[i].classList.contains("semPar")){ // se a carta semPar
+        
+                if(seletorCartas[i].querySelector("img").getAttribute("src") === 
+                carta.querySelector("img").getAttribute("src")){
+                    seletorCartas[i].classList.replace("semPar","comPar");
+                    carta.classList.add("comPar");
+                    numeroDeCartasSemPar = 0;
+                    
+                }
+                else if(seletorCartas[i].querySelector("img").getAttribute("src") !== 
+                carta.querySelector("img").getAttribute("src")) {
+                    seletorCartas[i].classList.remove("semPar");
+                    setTimeout(desvirarCarta, 1000, seletorCartas[i]);
+                    setTimeout(desvirarCarta, 1000, carta);
+                    numeroDeCartasSemPar = 0;
+                }
+            }
+        }
+    } 
+
+}
