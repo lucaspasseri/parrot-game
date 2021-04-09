744550924a08281753c41422bcec34ad8e93b6b8
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-04-09 03:14:58 -0300

    Tinha algo comentado errado no código.(2)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
  let numeroDeCartas = 
-prompt("Bem-vindo, com quantas cartas você gostaria de jogar? O Número inserido deve ser par entre 4 e 14."); */
+prompt("Bem-vindo, com quantas cartas você gostaria de jogar? O Número inserido deve ser par entre 4 e 14.");
 
 while(!(numeroDeCartas % 2 === 0 && numeroDeCartas > 3 && numeroDeCartas < 15)){
     numeroDeCartas = 
